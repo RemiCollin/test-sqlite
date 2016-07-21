eae2b69c7e721d9d18e5b809e9280561298c2e09
Rémi Collin <remi@plateau.io>
2016-07-21 10:59:06 +0200

    first commit

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use DB;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        
+        DB::table('test')->insert(
+            ['decimal' => 12345.12]
+        );
+        $test = DB::table('test')->get();
+
+        dd($test);
+    }
+}
